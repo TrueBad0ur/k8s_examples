f02012b2607554e898c9d8cff027bda71600c385
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2023-06-17 07:09:01 -0400

    Initial commit

---
+++ mywebapp/app.py
@@ -0,0 +1,11 @@
+import subprocess
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    out = subprocess.check_output("ip a | grep 'inet '", shell=True)
+    return out
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
